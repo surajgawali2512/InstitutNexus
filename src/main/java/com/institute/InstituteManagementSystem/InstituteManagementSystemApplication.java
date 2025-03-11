44e72efa1254cfe8c065151e7851f7d0d135b1f7
Nikhil Shinde <snikhil9282@gmail.com>
2025-03-11 15:54:17 +0530

    Initial commit

---
+++ src/main/java/com/institute/InstituteManagementSystem/InstituteManagementSystemApplication.java
@@ -0,0 +1,13 @@
+package com.institute.InstituteManagementSystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InstituteManagementSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InstituteManagementSystemApplication.class, args);
+	}
+
+}
